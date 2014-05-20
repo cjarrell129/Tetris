9568b2cb95f91c5a99aa239489d83f8efaa6b7bc
cjarrell129 <cjarrell129@gmail.com>
2014-05-19 23:24:38 -0500

    Commit at bottom of main_activity.java

--- Tetris/src/org/jarrellApps/tetris/MainActivity.java
+++ Tetris/src/org/jarrellApps/tetris/MainActivity.java
@@ -1,17 +1,32 @@
 package org.jarrellApps.tetris;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	//hey stupid i am trying a comment
+	public ArrayList<Integer> mLevelsCompleted = new ArrayList<Integer>();
+	public ArrayList<Integer> mLevelsRemaining = new ArrayList<Integer>();
+	public String mUser = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+Button startBtn = (Button) findViewById(R.id.startButton);
+		
+		startBtn.setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+					}
+				});
 	}
 
 	@Override
@@ -21,4 +36,41 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
 }
+//comment for you to see
